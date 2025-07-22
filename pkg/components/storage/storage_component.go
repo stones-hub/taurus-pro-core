5573e1ccf87e8d3eebe6c4c600fa9ca4ee3aeb43
STones_ <61647649@qq.com>
2025-07-22 17:51:46 +0800

    Optimization

--- pkg/components/storage/storage_component.go
+++ pkg/components/storage/storage_component.go
@@ -5,7 +5,7 @@ import "github.com/stones-hub/taurus-pro-core/pkg/components/types"
 var StorageComponent = types.Component{
 	Name:         "storage",
 	Package:      "github.com/stones-hub/taurus-pro-storage",
-	Version:      "v0.0.7",
+	Version:      "v0.0.8",
 	Description:  "DB,Redis存储组件",
 	IsCustom:     true,
 	Required:     false,
