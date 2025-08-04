95c374a3576bbe8e1095c6f411e0aa6a9ebb786a
STones_ <61647649@qq.com>
2025-08-04 21:04:58 +0800

    add

--- pkg/components/storage/db_component.go
+++ pkg/components/storage/db_component.go
@@ -25,6 +25,36 @@ func ProvideDbComponent(cfg *config.Config) (map[string]*gorm.DB, func(), error)
 	}
 
 	for _, dbOptions := range dbOptionsList {
+		// 日志地址
+		logPath := dbOptions["log_path"].(string)
+		if logPath == "" {
+			logPath = "./logs/db/db.log"
+		}
+
+		// 日志级别
+		logLevel := dbOptions["log_level"].(string)
+		level := logger.Info
+		switch logLevel {
+		case "info":
+			level = logger.Info
+		case "warn":
+			level = logger.Warn
+		case "error":
+			level = logger.Error
+		case "silent":
+			level = logger.Silent
+		}
+
+		// 日志格式
+		logFormatter := dbOptions["log_formatter"].(string)
+		formatter := db.DefaultLogFormatter
+		switch logFormatter {
+		case "json":
+			formatter = db.JSONLogFormatter
+		case "default":
+			formatter = db.DefaultLogFormatter
+		}
+
 		err := db.InitDB(db.WithMaxOpenConns(dbOptions["max_open_conns"].(int)),
 			db.WithMaxIdleConns(dbOptions["max_idle_conns"].(int)),
 			db.WithConnMaxLifetime(time.Duration(dbOptions["conn_max_lifetime"].(int))*time.Second),
@@ -34,9 +64,9 @@ func ProvideDbComponent(cfg *config.Config) (map[string]*gorm.DB, func(), error)
 			db.WithDBType(dbOptions["dbtype"].(string)),
 			db.WithDSN(dbOptions["dsn"].(string)),
 			db.WithLogger(db.NewDbLogger(
-				db.WithLogFilePath("./logs/db/db.log"),
-				db.WithLogLevel(logger.Info),
-				db.WithLogFormatter(db.JSONLogFormatter))),
+				db.WithLogFilePath(logPath),
+				db.WithLogLevel(level),
+				db.WithLogFormatter(formatter))),
 		)
 		if err != nil {
 			return nil, func() {}, err
@@ -53,13 +83,14 @@ func ProvideDbComponent(cfg *config.Config) (map[string]*gorm.DB, func(), error)
 }
 
 var dbWire = &types.Wire{
-	RequirePath:  []string{"github.com/stones-hub/taurus-pro-storage/pkg/db", "gorm.io/gorm", "gorm.io/gorm/logger", "log"},
+	RequirePath:  []string{"github.com/stones-hub/taurus-pro-storage/pkg/db", "gorm.io/gorm", "gorm.io/gorm/logger", "log", "time"},
 	Name:         "DbList",
 	Type:         "map[string]*gorm.DB",
 	ProviderName: "ProvideDbComponent",
 	Provider: `
 	func {{.ProviderName}}(cfg *config.Config) (map[string]*gorm.DB, func(), error) {
-	enable := cfg.GetBool("databases.enable")
+
+		enable := cfg.GetBool("databases.enable")
 
 	if !enable {
 		return nil, func() {}, nil
@@ -72,6 +103,36 @@ var dbWire = &types.Wire{
 	}
 
 	for _, dbOptions := range dbOptionsList {
+		// 日志地址
+		logPath := dbOptions["log_path"].(string)
+		if logPath == "" {
+			logPath = "./logs/db/db.log"
+		}
+
+		// 日志级别
+		logLevel := dbOptions["log_level"].(string)
+		level := logger.Info
+		switch logLevel {
+		case "info":
+			level = logger.Info
+		case "warn":
+			level = logger.Warn
+		case "error":
+			level = logger.Error
+		case "silent":
+			level = logger.Silent
+		}
+
+		// 日志格式
+		logFormatter := dbOptions["log_formatter"].(string)
+		formatter := db.DefaultLogFormatter
+		switch logFormatter {
+		case "json":
+			formatter = db.JSONLogFormatter
+		case "default":
+			formatter = db.DefaultLogFormatter
+		}
+
 		err := db.InitDB(db.WithMaxOpenConns(dbOptions["max_open_conns"].(int)),
 			db.WithMaxIdleConns(dbOptions["max_idle_conns"].(int)),
 			db.WithConnMaxLifetime(time.Duration(dbOptions["conn_max_lifetime"].(int))*time.Second),
@@ -81,9 +142,9 @@ var dbWire = &types.Wire{
 			db.WithDBType(dbOptions["dbtype"].(string)),
 			db.WithDSN(dbOptions["dsn"].(string)),
 			db.WithLogger(db.NewDbLogger(
-				db.WithLogFilePath("./logs/db/db.log"),
-				db.WithLogLevel(logger.Info),
-				db.WithLogFormatter(db.JSONLogFormatter))),
+				db.WithLogFilePath(logPath),
+				db.WithLogLevel(level),
+				db.WithLogFormatter(formatter))),
 		)
 		if err != nil {
 			return nil, func() {}, err
@@ -97,6 +158,7 @@ var dbWire = &types.Wire{
 		log.Printf("%s🔗 -> Clean up database components successfully. %s\n", "\033[32m", "\033[0m")
 
 	}, nil
+	
 }
 `,
 }
