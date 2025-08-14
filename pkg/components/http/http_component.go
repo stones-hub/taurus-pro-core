8d49f896c701bcfc0ee9be4bb1b05db301033ad7
STones_ <61647649@qq.com>
2025-08-14 12:52:26 +0800

    fixed: cors 校验

--- pkg/components/http/http_component.go
+++ pkg/components/http/http_component.go
@@ -122,7 +122,7 @@ var mcpWire = &types.Wire{
 var HttpComponent = types.Component{
 	Name:         "http",
 	Package:      "github.com/stones-hub/taurus-pro-http",
-	Version:      "v0.0.17",
+	Version:      "v0.1.1",
 	Description:  "Http,WebSocket服务器组件",
 	IsCustom:     true,
 	Required:     true,
